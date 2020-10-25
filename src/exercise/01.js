c8f4f8aad342385941bb585323ecbbd49a8558b2
Luigi <luigi@able.co>
2020-10-24 23:22:20 -0500

    Hooks first progress

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,12 +3,13 @@
 
 import React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    event.preventDefault()
+    const {value: username} = event.target
+    setName(username)
   }
 
   return (
@@ -23,7 +24,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Luigi" />
 }
 
 export default App
